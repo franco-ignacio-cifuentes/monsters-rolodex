d14d34cf689c3138bd6f29da1abc850067fd07e8
Franco Cifuentes <franco.cifuentes@norcal.aaa.com>
2022-12-21 12:05:28 -0300

    61. Monsters Rolodex - Remaining Components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,22 +1,29 @@
-import { Component } from "react";
 import Card from "../card/card.component";
 
 import './card-list.styles.css';
 
-class CardList extends Component {
-    render() {
-        const { monsters } = this.props;
+const CardList = ({ monsters }) => (
+    <div className="card-list">
+        {monsters.map((monster) => {
+            return <Card monster={monster} key={monster.id} />;
+        })}
+    </div>
+)
 
-        return (
-            <div className="card-list">
-                {monsters.map((monster) => {
-                    return (
-                        <Card monster={monster} key={monster.id}/>
-                    )
-                })}
-            </div>
-        )
-    }
-}
+// class CardList extends Component {
+//     render() {
+//         const { monsters } = this.props;
+
+//         return (
+//             <div className="card-list">
+//                 {monsters.map((monster) => {
+//                     return (
+//                         <Card monster={monster} key={monster.id}/>
+//                     )
+//                 })}
+//             </div>
+//         )
+//     }
+// }
 
 export default CardList;
